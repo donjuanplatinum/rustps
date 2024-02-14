ee5cd28d3679a10205554474d3962b8b092cdeb8
donjuanplatinum <donjuan@lecturify.net>
2024-02-14 12:42:22 +0800

    fix \0

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use rustps::ProcessStat;
 use rustps::get_cpu_frequency;
 use rustps::format_process_stats;
 use rustps::load_process_stats;
+use rustps::handle_options;
 fn main() {
-    if let Ok(process_stats) = load_process_stats() {
-        format_process_stats(&process_stats);
-    } else {
-        eprintln!("Failed to load process stats.");
-    }
+    // if let Ok(process_stats) = load_process_stats() {
+    //     format_process_stats(&process_stats);
+    // } else {
+    //     eprintln!("Failed to load process stats.");
+    // }
+    let args: Vec<String> = env::args().collect();
+    handle_options(args);
 }
 
 
